b4f5bce43fc19f2b7fa693fe41ca23dcfab38cc4
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-02-28 10:13:22 -0700

    Copy activation to tanstack queries

--- projects/packages/my-jetpack/_inc/data/types.ts
+++ projects/packages/my-jetpack/_inc/data/types.ts
@@ -118,3 +118,5 @@ export type ProductCamelCase = {
 	title: string;
 	wpcomProductSlug: string;
 };
+
+export type ProductSnakeCase = Window[ 'myJetpackInitialState' ][ 'products' ][ 'items' ][ string ];
